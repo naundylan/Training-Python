962cb4776e37b49efb9000bef301a35e9a3fa707
quyen <naundylan@gmail.com>
2024-09-19 21:10:01 +0700

    first commit

---
+++ day01/practice.py
@@ -0,0 +1,35 @@
+################ print(object, sep, end)
+
+# default
+print('hello')
+# sep = ',' & end = "hehe"
+a = 10
+b = 20
+c = 30
+print(a, b ,c ,sep = ',', end = "hehe")
+
+
+
+
+
+################# comment in python
+
+# this is a comment in python
+""" this is a paragraph in python
+this line too"""
+''' this type too'''
+
+
+
+
+################# variable type
+
+one = 2
+two = "hello"
+three = True
+print(type(two))
+print(type(three))
+# number type: int, float, complex number
+x = 1
+y = 1.1
+z = 1 + 2j
