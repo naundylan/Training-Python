cdfa70ab5ec7dae7a93637a2549e83b28122fcc5
quyen <naundylan@gmail.com>
2024-11-08 18:26:48 +0700

    update hackerrank

---
+++ Python - HackerRank/week1/p10.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
