99101a77f1c759b9707c5459d7bb4864a7a04ab8
quyen <naundylan@gmail.com>
2024-11-22 21:56:48 +0700

    update hackerrank

--- Python - HackerRank/week1/find_a_string.py
+++ Python - HackerRank/week1/find_a_string.py
@@ -1,10 +1,12 @@
 def count_substring(string, sub_string):
     cnt = 0
     for i in range(len(string) - len(sub_string) + 1):
-        if sub_string[0] == string[i]:
-            for j in range(len(sub_string)):
-                if string[i + j] != sub_string[j]:
-                    break
+        status = True
+        for j in range(len(sub_string)):
+            if string[i + j] != sub_string[j]:
+                status = False
+                break
+        if status:
             cnt += 1
     return cnt
 
