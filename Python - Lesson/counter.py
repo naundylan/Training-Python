0fe67e4e9bea33f397ff79e43b5748290577da3d
quyen <naundylan@gmail.com>
2025-01-05 10:05:28 +0700

    update review

---
+++ Python - Lesson/counter.py
@@ -0,0 +1,4 @@
+from collections import *
+
+array = [1, 2, 3, 4, 5, 0, 1, 2, 3, 5, 0, 1, 2, 5, 0, 7, 4, 2]
+print(dict((Counter(array))))
---
+++ Python - Lesson/demo.py
@@ -0,0 +1,7 @@
+import datetime as dt
+
+date_object = dt.datetime.now()
+print(date_object)  
+
+date_time = dt.date.today()
+print(date_time)
